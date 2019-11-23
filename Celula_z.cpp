3439e107d4e0f891f50dd10dfec274aeb3b18e92
jnemi <51536906+jnemi@users.noreply.github.com>
2019-11-23 03:06:44 +0300

    Metodo de contagio corregido

--- Celula_z.cpp
+++ Celula_z.cpp
@@ -15,22 +15,6 @@ Celula_z::~Celula_z(){
   //cout << "Destruyendo Celula Z -> ";
 }
 
-void Celula_z::contagiar(Lista<Celula>* lista_celulas)
-{
- //CODIGO DE CONTAGIO
- cout<<"Preparando contagio ";
- srand(time(NULL));
- int indice = 1 + rand()%(adyacentes.obtener_largo());
- Celula* cel_objetivo = lista_celulas->obtener_puntero(adyacentes.obtener_valor(indice));
- if (cel_objetivo->obtener_tipo_celula() == 's'){
-    Celula* contagiada = new Celula_x;
-    for (int i = 1; i <= cel_objetivo->obtenerCantidadAdyacentes(); i++)
-        contagiada->agregarAdyacente(cel_objetivo->obtenerAdyacente(i));
-    lista_celulas->reemplazar(indice, contagiada);
-    cout<<"Contagiando "<<indice<<endl;
- }
-}
-
 //Setter
 void Celula_z::asignarPorcentajeMutacion(float pm){
   porcentajeMutacion = pm;
